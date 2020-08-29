e3559d94513115bbd0d8a768fd4ff9e9c4206dbc
mcj010 <329392494@qq.com>
2020-08-29 22:32:45 +0800

    add bus

--- src/main/java/com/example/configclient/ConfigclientApplication.java
+++ src/main/java/com/example/configclient/ConfigclientApplication.java
@@ -2,8 +2,13 @@ package com.example.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+//@RefreshScope
+@EnableDiscoveryClient 
 public class ConfigclientApplication {
 
 	public static void main(String[] args) {
